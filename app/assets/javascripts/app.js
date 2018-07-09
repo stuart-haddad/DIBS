c14a6af8d093c499c3e55565d0aaa350f5ea2017
Matt <mattvroom1@gmail.com>
2018-07-09 12:37:46 -0500

    Change from ES6 to work with Heroku

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -1,9 +1,10 @@
 window.onload = function() {
 
 var panel = document.getElementsByClassName('welcome-panel');
-var pic = document.getElementById('image').onclick = () => {
+var pic = document.getElementById('image').onclick = function() {
 	for (var i=0;i<panel.length;i+=1){
   		panel[i].style.display = 'none';
 	}
 };
 };
+
