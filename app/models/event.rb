e37cad8696e9f5ed242e42c973acaa1aa97abdcf
Stuart Haddad <stuart.haddad@gmail.com>
2018-07-09 16:25:09 -0500

    Hover on Welcome Panel to test animation

--- app/models/event.rb
+++ app/models/event.rb
@@ -112,7 +112,7 @@ class Event
 
   def parse_welcome(summary)
      if summary.include?("[") == false
-      return "Welcome! Let's get started."
+      return "Welcome! Let's get started"
   else
     return "Welcome, "
     end
