704493b0be41db306c1eaff18ac0c607ec7c5c8a
Stuart Haddad <stuart.haddad@gmail.com>
2018-07-11 16:41:37 -0500

    Disappearing Welcome Panel Fix

--- app/assets/javascripts/app.js
+++ app/assets/javascripts/app.js
@@ -3,6 +3,7 @@ window.onload = function() {
 	jQuery('.animate-on-load').addClass('slide-out');
 	//On click of X, remove slide out class
 	jQuery("#close-welcome-message").click(function(){
+		jQuery('.welcome-panel').removeClass('stay-there');
 		jQuery('.welcome-panel').removeClass('slide-out');
 	});
 };
