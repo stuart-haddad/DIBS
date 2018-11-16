09d8cb736cf742a16b9350f1c282723c273e6ce9
Nicholas Defelice <nick.d@threesixtyeight.is>
2018-11-16 15:58:04 -0600

    Test iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  before_filter :default_headers
+  #before_filter :default_headers
 
   #http_basic_authenticate_with name: ENV["BASIC_AUTH_USER_NAME"], password: ENV["BASIC_AUTH_PASSWORD"]
 
