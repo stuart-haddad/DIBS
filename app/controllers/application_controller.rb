c145adf106c3aa8de83ac4add031fb506a46021d
Nicholas Defelice <nick.d@threesixtyeight.is>
2018-11-16 16:06:59 -0600

    Test iframe

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
-  #before_filter :default_headers
+  before_filter :default_headers
 
   #http_basic_authenticate_with name: ENV["BASIC_AUTH_USER_NAME"], password: ENV["BASIC_AUTH_PASSWORD"]
 
@@ -9,6 +9,6 @@ class ApplicationController < ActionController::Base
   end
 
   def default_headers
-	  headers['X-Frame-Options'] = 'allow-from https://signage.screen.cloud'
+	  headers['X-Frame-Options'] = 'allow-from http://signage-assets.screen.cloud'
 	end
 end
