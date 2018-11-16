695f8842153172281190ad8a33762be813aa3358
Nicholas Defelice <nick.d@threesixtyeight.is>
2018-11-16 15:55:31 -0600

    remove http auth

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,7 +2,7 @@ class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
   before_filter :default_headers
 
-  http_basic_authenticate_with name: ENV["BASIC_AUTH_USER_NAME"], password: ENV["BASIC_AUTH_PASSWORD"]
+  #http_basic_authenticate_with name: ENV["BASIC_AUTH_USER_NAME"], password: ENV["BASIC_AUTH_PASSWORD"]
 
   rescue_from Google::Apis::AuthorizationError, Google::Apis::ClientError do
     redirect_to authorize_url
